7eaed6f9773b8e7fdf5bda7d2e9ab0610bb98ad1
Steven Lim <limstevenlbw@gmail.com>
2019-04-16 11:24:24 -0700

    Pizza Project Completed

--- pizzaproject/rb_build.js
+++ pizzaproject/rb_build.js
@@ -5,98 +5,20 @@
 */
 
 
+   /* Object literal that stores the price of the pizza items */
+   const pizzaPrices = {
+      size12: 11,
+      size14: 13,
+      size16: 16,
+      thin: 0,
+      thick: 0,
+      stuffed: 3,
+      pan: 2,
+      dsauce: 1.5,
+      dcheese: 1.5,
+      topping: 1.5,
+   };
 
-/**DOM ELEMENT ASSIGNMENTS */
-// Preview image of the pizza 
-let pizzaPreviewBox = document.getElementById("previewBox");
-// Summary of the pizza order
-let pizzaSummary = document.getElementById("pizzaSummary");
-// Pizza size selection list
-let pizzaSizeBox = document.getElementById("pizzaSize");
-// Pizza crust selection list
-let pizzaCrustBox = document.getElementById("pizzaCrust");
-// Pizza double sauce checkbox
-let pizzaDoubleSauceBox = document.getElementById("doubleSauce");
-// Pizza double cheese checkbox
-let pizzaDoubleCheeseBox = document.getElementById("doubleCheese");
-// Pizza topping option buttons
-let toppingOptions = document.getElementsByClassName("topping");
-// Pizza quantity selection list
-let pizzaQuantityBox = document.getElementById("pizzaQuantity");
-// Add to cart button
-let addToCartButton = document.getElementById("addToCart");
-// Order table displaying the items in the shopping cart
-let cartTableBody = document.getElementById("cartTable");
-// Shopping cart total box
-let cartTotalBox = document.getElementById("cartTotal");
-
-/* Object literal that stores the price of the pizza items */
-let pizzaPrices = {
-   size12: 11,
-   size14: 13,
-   size16: 16,
-   thin: 0,
-   thick: 0,
-   stuffed: 3,
-   pan: 2,
-   dsauce: 1.5,
-   dcheese: 1.5,
-   topping: 1.5,
-};
-
-//Create a shopping cart
-let myCart = new cart();
-
-/* Constructor for the class of pizza objects */
-function pizza(size, crust, dsauce, dcheese) {
-   this.size = size;
-   this.crust = crust;
-   this.dsauce = dsauce;
-   this.dcheese = dcheese;
-   this.topping = []
-}
-
-/* Method to add a topping to a pizza */
-pizza.prototype.addTopping = function (topping) {
-   this.toppings.push(topping);
-};
-
-/* Method to calculate the cost of a pizza item */
-pizza.prototype.calcPizzaPrice = function () {
-
-   if (this.size === "12") { this.price = pizzaPrices.size12; }
-   else if (this.size === "14") { this.price = pizzaPrices.size14; }
-   else if (this.size === "16") { this.price = pizzaPrices.size16; }
-
-   if (this.crust === "stuffed") { this.price += pizzaPrices.stuffed; }
-   else if (this.crust === "thin") { this.price += pizzaPrices.thin }
-   else if (this.crust === "pan") { this.price += pizzaPrices.pan; }
-   else if (this.crust === "thick") { this.price += pizzaPrices.thick; }
-
-   if (this.dsauce) { this.price += pizzaPrices.dsauce; }
-   if (this.dcheese) { this.price += pizzaPrices.dcheese; }
-
-   for (let i = 0; i < this.toppings.length; i++) {
-      this.price += (this.toppings[i].qty * pizzaPrices.topping);
-   }
-
-   return this.price;
-};
-
-/* Constructor for the class of cart objects */
-function cart() {
-   this.totalCost = 0;
-   this.items = [];
-}
-/* Method to return the total cost of the items in the cart */
-cart.prototype.calcCartTotal = function () {
-   let cartTotal = 0;
-   this.items.forEach(function (item) {
-      cartTotal += item.calcItemCost;
-   });
-   this.totalCost = cartTotal;
-   return this.totalCost;
-};
 
 /* Constructor for individual food items */
 function cartItem(qty, price) {
@@ -115,178 +37,261 @@ cartItem.prototype.addToCart = function (cart) {
 cartItem.prototype.removeFromCart = function (cart) {
    for (let i = 0; i < cart.items.length; i++) {
       if (this === cart.items[i]) {
-         cart.items.splice(this, 1);
+         cart.items.splice(i, 1);
          break;
       }
    }
 };
+
+/* Constructor for the class of pizza objects */
+function pizza(size, crust, dsauce, dcheese) {
+   this.size = size;
+   this.crust = crust;
+   this.dsauce = dsauce;
+   this.dcheese = dcheese;
+   this.topping = []
+}
+/* Pizza and topping inherit from the item class */
+pizza.prototype = new cartItem();
+
+/* Method to add a topping to a pizza */
+pizza.prototype.addTopping = function (topping) {
+   this.topping.push(topping);
+};
+
+/* Method to calculate the cost of a pizza item */
+pizza.prototype.calcPizzaPrice = function () {
+
+   if (this.size === "12") { this.price = pizzaPrices.size12; }
+   else if (this.size === "14") { this.price = pizzaPrices.size14; }
+   else if (this.size === "16") { this.price = pizzaPrices.size16; }
+ 
+   if (this.crust === "stuffed") { this.price += pizzaPrices.stuffed; }
+   else if (this.crust === "thin") { this.price += pizzaPrices.thin; }
+   else if (this.crust === "pan") { this.price += pizzaPrices.pan; }
+   else if (this.crust === "thick") { this.price += pizzaPrices.thick; }
+  
+   if (this.dsauce) { this.price += pizzaPrices.dsauce; }
+   if (this.dcheese) { this.price += pizzaPrices.dcheese; }
+
+   for (let i = 0; i < this.topping.length; i++) {
+      this.price += (this.topping[i].qty * pizzaPrices.topping);
+   }
+   return this.price*this.qty;
+};
+
+/* Constructor for the class of cart objects */
+function cart() {
+   this.totalCost = 0;
+   this.items = [];
+}
+/* Method to return the total cost of the items in the cart */
+cart.prototype.calcCartTotal = function () {
+   let cartTotal = 0;
+   this.items.forEach(function (item) {
+      cartTotal += item.calcItemCost();
+
+   });
+   this.totalCost = cartTotal;
+   return this.totalCost;
+};
+
 
 /* Constructor for the class of pizza toppings */
 function topping(name, side) {
    this.name = name;
    this.side = side;
 }
-
-
-// Function to build the pizza, acquires the selected values from the user
-function buildPizza(newPizza) {
-   newPizza.qty = pizzaQuantityBox.value;
-   newPizza.size = pizzaSizeBox.selectedValue;
-   newPizza.crust = pizzaCrustBox.selectedValue;
-   newPizza.dsauce = pizzaDoubleSauceBox.selectedValue;
-   newPizza.dcheese = pizzaDoubleCheeseBox.selectedValue;
-
-   let checkedToppings = document.querySelectorAll("input.topping:checked");
-   for (let i = 0; i < checkedToppings.length; i++) {
-      if (checkedToppings[i].value !== "none") {
-         let myTopping = new topping();
-         myTopping.name = checkedToppings[i].parentNode.firstChild.value;
-         myTopping.side = checkedToppings[i].value;
-
-         if (checkedToppings[i].value === "full") {
-            //   x = 1;
-         }
-         else {
-            //    x = 0.5;
-         }
-         newPizza.topping.push(myTopping);
-      }
-
-   }
-
-}
-
-
-/* Pizza and topping inherit from the item class */
-pizza.prototype = new cartItem();
 topping.prototype = new cartItem();
 
-// Event handlers to draw the pizza image
-pizzaSizeBox.onchange = drawPizza;
-pizzaCrustBox.onchange = drawPizza;
-pizzaDoubleSauceBox.onclick = drawPizza;
-pizzaDoubleCheeseBox.onclick = drawPizza;
-pizzaQuantityBox.onchange = drawPizza;
-for (let i = 0; i < toppingOptions.length; i++) {
-   toppingOptions[i].onclick = drawPizza;
-}
+window.addEventListener("load", function() {
+   /**DOM ELEMENT ASSIGNMENTS */
 
-addToCartButton.onclick = addPizzaToCart;
+   // Preview image of the pizza 
+   let pizzaPreviewBox = document.getElementById("previewBox");
+   // Summary of the pizza order
+   let pizzaSummary = document.getElementById("pizzaSummary");
+   // Pizza size selection list
+   let pizzaSizeBox = document.getElementById("pizzaSize");
+   // Pizza crust selection list
+   let pizzaCrustBox = document.getElementById("pizzaCrust");
+   // Pizza double sauce checkbox
+   let pizzaDoubleSauceBox = document.getElementById("doubleSauce");
+   // Pizza double cheese checkbox
+   let pizzaDoubleCheeseBox = document.getElementById("doubleCheese");
+   // Pizza topping option buttons
+   let toppingOptions = document.getElementsByClassName("topping");
+   // Pizza quantity selection list
+   let pizzaQuantityBox = document.getElementById("pizzaQuantity");
+   // Add to cart button
+   let addToCartButton = document.getElementById("addToCart");
+   // Order table displaying the items in the shopping cart
+   let cartTableBody = document.getElementById("cartTable");
+   // Shopping cart total box
+   let cartTotalBox = document.getElementById("cartTotal");
 
-// Function to add the built pizza to the shopping cart
-function addPizzaToCart() {
-   let myPizza = new pizza();
-   buildPizza(myPizza);
-   myPizza.addToCart(myCart);
+   //Create a shopping cart
+   let myCart = new cart();
 
-   let newItemRow = document.createElement("tr");
-   cartTableBody.appendChild(newItemRow);
+   // Event handlers to draw the pizza image
+   pizzaSizeBox.onchange = drawPizza;
+   pizzaCrustBox.onchange = drawPizza;
+   pizzaDoubleSauceBox.onclick = drawPizza;
+   pizzaDoubleCheeseBox.onclick = drawPizza;
+   pizzaQuantityBox.onchange = drawPizza;
+   for (let i = 0; i < toppingOptions.length; i++) {
+      toppingOptions[i].onclick = drawPizza;
+   }
 
-   let summaryCell = document.createElement("td");
-   summaryCell.textContent = pizzaSummary.textContent;
-   newItemRow.appendChild(summaryCell);
+   addToCartButton.onclick = addPizzaToCart;
 
-   let qtyCell = document.createElement("td");
-   qtyCell.textContent = myPizza.qty;
-   newItemRow.appendChild(qtyCell);
+   // Function to build the pizza, acquires the selected values from the user
+   function buildPizza(newPizza) {
+      newPizza.qty = pizzaQuantityBox.value;
+      newPizza.size = pizzaSizeBox.value;
+      newPizza.crust = pizzaCrustBox.value;
+      newPizza.dsauce = pizzaDoubleSauceBox.checked;
+      newPizza.dcheese = pizzaDoubleCheeseBox.checked;
 
-   let priceCell = document.createElement("td");
-   priceCell.textContent = 4;
+      let checkedToppings = document.querySelectorAll("input.topping:checked");
+      for (let i = 0; i < checkedToppings.length; i++) {
+         if (checkedToppings[i].value !== "none") {
+            let myTopping = new topping();
+            myTopping.name = checkedToppings[i].name;
+            myTopping.side = checkedToppings[i].value;
 
-   newItemRow.append(priceCell);
+            if (checkedToppings[i].value === "full") {
+               myTopping.qty = 1;
+            }
+            else {
+               myTopping.qty = 0.5;
+            }
+            newPizza.topping.push(myTopping);
+         }
 
-   let removeCell = document.createElement("td");
-   let removeButton = document.createElement("input");
-   removeButton.value = "X";
-   removeButton.type = "button";
-   removeCell.textContent = removeButton;
-   newItemRow.append(removeCell);
+      }
 
-   cartTotalBox.value = myCart.calcCartTotal;
+   }
 
-   console.log(myCart);
+   // Function to add the built pizza to the shopping cart
+   function addPizzaToCart() {
+      let myPizza = new pizza();
+      buildPizza(myPizza);
+      myPizza.addToCart(myCart);
+      let newItemRow = document.createElement("tr");
+      cartTableBody.appendChild(newItemRow);
 
-   removeButton.onclick = function () {
-      myPizza.removeFromCart(myCart);
-      cartTableBody.removeChild(newItemRow);
-      cartTotalBox.value = myCart.calcCartTotal;
-      console.log(myCart);
-   };
+      let summaryCell = document.createElement("td");
+      summaryCell.textContent = pizzaSummary.textContent;
+      newItemRow.appendChild(summaryCell);
 
-   resetDrawPizza();
-}
+      let qtyCell = document.createElement("td");
+      qtyCell.textContent = myPizza.qty;
+      newItemRow.appendChild(qtyCell);
 
-function drawPizza() {
-   pizzaPreviewBox.removeChildren;
-   let pizzaDescription = "";
+      let priceCell = document.createElement("td");
+      priceCell.textContent = myPizza.calcPizzaPrice().toLocaleString('en-US', {style: "currency", currency: "USD"});
 
-   pizzaDescription += pizzaSizeBox.selectedValue() + '" pizza ';
-   pizzaDescription += pizzaCrustBox.selectedValue() + ", ";
-   if (pizzaDoubleSauceBox.checked) {
+      newItemRow.append(priceCell);
+
+      let removeCell = document.createElement("td");
+      let removeButton = document.createElement("input");
+      removeButton.value = "X";
+      removeButton.type = "button";
+      removeCell.append(removeButton);
+      newItemRow.append(removeCell);
+
+      cartTotalBox.value = myCart.calcCartTotal().toLocaleString('en-US', {style: "currency", currency: "USD"});
+
+      removeButton.onclick = function () {
+         myPizza.removeFromCart(myCart);
+         cartTableBody.removeChild(newItemRow);
+         cartTotalBox.value = myCart.calcCartTotal().toLocaleString('en-US', {style: "currency", currency: "USD"});
+      };
+
+      resetDrawPizza();
+   }
+
+   function drawPizza() {
+      pizzaPreviewBox.removeChildren;
+      let pizzaDescription = "";
+      pizzaDescription += pizzaSizeBox.selectedValue() + '" pizza, ';
+      pizzaDescription += pizzaCrustBox.selectedValue();
+
       let sauceImg = document.createElement("img");
-      sauceImg.src = "rb_doublesauce.png";
-      pizzaPreviewBox.appendChild(sauceImg);
-      pizzaDescription += "double sauce, ";
-   }
-   if (pizzaDoubleCheeseBox.checked) {
       let cheeseImg = document.createElement("img");
-      cheeseImg.src = "rb_doublecheese.png";
-      pizzaPreviewBox.appendChild(cheeseImg);
-      pizzaDescription += "double cheese, ";
-
-   }
-
-   let checkedToppings = document.querySelectorAll("input.topping:checked");
-   for (let i = 0; i < checkedToppings.length; i++) {
-      let toppingImage = document.createElement('img');
-      let removableNode = document.getElementById("" + checkedToppings[i].name + "Preview");
-      if (checkedToppings[i].value !== "none") {
-         console.log("hello");
-         pizzaDescription += checkedToppings[i].name + "(" + checkedToppings[i].value + "), ";
-         toppingImage.id = "" + checkedToppings[i].name + "Preview";
-         toppingImage.src = "./rb_" + checkedToppings[i].name + ".png";
-
-         if (removableNode)
-            pizzaPreviewBox.removeChild(removableNode);
-
-         pizzaPreviewBox.appendChild(toppingImage);
-
-
-         if (checkedToppings[i].value === "left") {
-            toppingImage.style.clip = "rect(0px, 150px, 300px, 0px)";
-         }
-         else if (checkedToppings[i].value === "right") {
-            toppingImage.style.clip = "rect(0px, 300px, 300px, 150px)";
-         }
+      let removableSauce = document.getElementById("doubleSaucePreview");
+      let removableCheese = document.getElementById("doubleCheesePreview");
+      if (pizzaDoubleSauceBox.checked && !removableSauce) {
+         sauceImg.id = "doubleSaucePreview";
+         sauceImg.src = "rb_doublesauce.png";
+         pizzaPreviewBox.appendChild(sauceImg);
+         pizzaDescription += ", double sauce";
       }
-      else {
-         if (removableNode)
-            pizzaPreviewBox.removeChild(removableNode);
+      else if(!pizzaDoubleSauceBox.checked && removableSauce){
+         pizzaPreviewBox.removeChild(removableSauce);
       }
 
+      if (pizzaDoubleCheeseBox.checked && !removableCheese) {
+         cheeseImg.id = "doubleCheesePreview";
+         cheeseImg.src = "rb_doublecheese.png";
+         pizzaPreviewBox.appendChild(cheeseImg);
+         pizzaDescription += ", double cheese";
+      }
+      else if (!pizzaDoubleCheeseBox.checked && removableCheese){
+         pizzaPreviewBox.removeChild(removableCheese);
+      }
+      
+      let checkedToppings = document.querySelectorAll("input.topping:checked");
+      for (let i = 0; i < checkedToppings.length; i++) {
+         let toppingImage = document.createElement('img');
+         let removableNode = document.getElementById("" + checkedToppings[i].name + "Preview");
+         if (checkedToppings[i].value !== "none") {
 
-      pizzaSummary.textContent = pizzaDescription;
+            pizzaDescription += ", " + checkedToppings[i].name + "(" + checkedToppings[i].value + ")";
+            toppingImage.id = checkedToppings[i].name + "Preview";
+            toppingImage.src = "./rb_" + checkedToppings[i].name + ".png";
+
+            if (removableNode)
+               pizzaPreviewBox.removeChild(removableNode);
+
+            pizzaPreviewBox.appendChild(toppingImage);
+
+
+            if (checkedToppings[i].value === "left") {
+               toppingImage.style.clip = "rect(0px, 150px, 300px, 0px)";
+            }
+            else if (checkedToppings[i].value === "right") {
+               toppingImage.style.clip = "rect(0px, 300px, 300px, 150px)";
+            }
+         }
+         else if (removableNode){
+               pizzaPreviewBox.removeChild(removableNode);
+         }
+
+
+         pizzaSummary.textContent = pizzaDescription;
+      }
    }
-}
 
-// Function to reset the pizza drawing 
-function resetDrawPizza() {
-   // Object collection of all topping option buttons with a value of 'none'
-   var noTopping = document.querySelectorAll("input.topping[value='none']");
+   // Function to reset the pizza drawing 
+   function resetDrawPizza() {
+      // Object collection of all topping option buttons with a value of 'none'
+      let noTopping = document.querySelectorAll("input.topping[value='none']");
+      pizzaSizeBox.selectedIndex = 1;
+      pizzaCrustBox.selectedIndex = 0;
+      pizzaDoubleSauceBox.checked = false;
+      pizzaDoubleCheeseBox.checked = false;
 
-   pizzaSizeBox.selectedIndex = 0;
-   pizzaCrustBox.selectedIndex = 0;
-   pizzaDoubleSauceBox.checked = false;
-   pizzaDoubleCheeseBox.checked = false;
-
-   for (var i = 0; i < noTopping.length; i++) {
-      noTopping[i].checked = false;
+      for (let i = 0; i < noTopping.length; i++) {
+         noTopping[i].checked = true;
+      }
+      pizzaSummary.textContent = "";
+      pizzaPreviewBox.removeChildren();
+      drawPizza();
+      pizzaQuantityBox.selectedIndex = 0;
    }
-   pizzaSummary.textContent = "";
-   pizzaPreviewBox.removeChildren;
-   pizzaQuantityBox.selectedIndex = 0;
-}
-
+});
 
 /*-------------------- Custom Methods --------------------*/
 
